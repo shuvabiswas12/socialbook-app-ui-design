8d9b41b241fccc22bdc05f5c384761df9c01d71e
Shuva_Biswas <shuvabiswas12@gmail.com>
2021-07-18 12:04:47 +0600

    user-settings added.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let settingsMenu = document.querySelector(".settings-menu")
+console.log(settingsMenu)
+function settingsMenuToggle() {
+    console.log("Clicked")
+    settingsMenu.classList.toggle("settings-menu-height")
+}
